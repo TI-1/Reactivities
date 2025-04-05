a6dd493d08c011640558892ba40e5262cce3c763
Tobi <tibuoye@yahoo.co.uk>
2025-04-05 14:39:23 +0100

    End of section 7

--- client/src/features/activities/details/ActivityDetail.tsx
+++ client/src/features/activities/details/ActivityDetail.tsx
@@ -1,28 +1,45 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
-import { useActivities } from "../../../lib/types/hooks/UseActivities"
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { Link, useNavigate, useParams } from "react-router";
+import { useActivities } from "../../../lib/types/hooks/UseActivities";
 
-type Props = {
-  selectedActivity: Activity
-  cancelSelectActivity: () => void
-  openForm: (id: string) => void
-}
-
-export default function ActivityDetail({ selectedActivity, cancelSelectActivity, openForm }: Props) {
-  const { activities } = useActivities()
-  const activity = activities?.find(x => x.id === selectedActivity.id)
-  if (!activity) return <Typography>Loading...</Typography>
+export default function ActivityDetail() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { activity, isLoadingActivity } = useActivities(id);
+  if (isLoadingActivity) return <Typography>Loading...</Typography>;
+  if (!activity) return <Typography>Activity not found</Typography>;
   return (
     <Card>
-      <CardMedia component='img' src={`/images/categoryImages/${activity.category}.jpg`} />
+      <CardMedia
+        component="img"
+        src={`/images/categoryImages/${activity.category}.jpg`}
+      />
       <CardContent>
         <Typography variant="h5">{activity.title}</Typography>
-        <Typography variant="subtitle1" fontWeight='light'>{activity.date}</Typography>
+        <Typography variant="subtitle1" fontWeight="light">
+          {activity.date}
+        </Typography>
         <Typography variant="body1">{activity.description}</Typography>
       </CardContent>
       <CardActions>
-        <Button color="primary" onClick={() => openForm(activity.id)}>Edit</Button>
-        <Button onClick={cancelSelectActivity} color="inherit">Cancel</Button>
+        <Button
+          component={Link}
+          to={`/manage/${activity.id}`}
+          color="primary"
+        >
+          Edit
+        </Button>
+        <Button onClick={() => navigate("/activities")} color="inherit">
+          Cancel
+        </Button>
       </CardActions>
     </Card>
-  )
+  );
 }
