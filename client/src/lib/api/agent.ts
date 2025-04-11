dd0e2e6ac0d95cdf4376b1087df306e099a266cf
Tobi <tibuoye@yahoo.co.uk>
2025-04-12 00:42:40 +0100

    End of section 11

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -20,7 +20,7 @@ agent.interceptors.request.use((config) => {
 agent.interceptors.response.use(
   async (response) => {
     await sleep(1000);
-
+    store.uiStore.isIdle();
     return response;
   },
   async (error) => {
